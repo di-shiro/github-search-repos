821216a8d606e97271fc7699c8c488a33dbb1ba9
di-shiro <d.i.shiro@gmail.com>
2020-09-12 09:48:29 +0900

    GitHubの検索機能を追加 : formのinputタグに入力した文字（キーワード）をGraphQL query でGitHubから検索する

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import React, { useState } from 'react';
-import { ApolloProvider } from 'react-apollo'
-import { Query } from 'react-apollo'
+import React, { useState } from "react";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
 
-import client from './client'
-import { SEARCH_REPOSITORIES } from './graphql'
+import client from "./client";
+import { SEARCH_REPOSITORIES } from "./graphql";
 
-const VARIABLES = {
-  "after": null,
-  "before": null,
-  "first": 5,
-  "last": null,
-  "query": "フロントエンドエンジニア"
-}
+const INITIAL_STATE = {
+  after: null,
+  before: null,
+  first: 5,
+  last: null,
+  query: "フロントエンドエンジニア",
+};
 
-function App () {
-  const [paginationArgs, setPaginationArgs] = useState( VARIABLES )
+function App() {
+  const [paginationArgs, setPaginationArgs] = useState(INITIAL_STATE);
+
+  // classコンポーネントの場合
+  // this.handleChange = this.handleChange.bind( this )
+  const handleChange = (event) => {
+    setPaginationArgs({
+      ...INITIAL_STATE,
+      query: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const { query, first, last, before, after } = paginationArgs;
+  console.log({ query });
 
   return (
     <ApolloProvider client={client}>
       <div>Hello, GraphQL</div>
 
+      <form onSubmit={handleSubmit}>
+        <input value={query} onChange={handleChange} />
+      </form>
+
       <Query query={SEARCH_REPOSITORIES} variables={{ ...paginationArgs }}>
-        {
-          ( { loading, error, data } ) => {
-            if ( loading ) return `Loading...`
-            if ( error ) return `Error! &{error.message}`
+        {({ loading, error, data }) => {
+          if (loading) return `Loading...`;
+          if (error) return `Error! &{error.message}`;
 
-            console.log( { data } )
+          console.log({ data });
 
-            return <div></div>
-          }
-        }
+          return <div></div>;
+        }}
       </Query>
     </ApolloProvider>
   );
